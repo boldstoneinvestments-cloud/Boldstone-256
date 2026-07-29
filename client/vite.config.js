5dd3cd60ed99f34bac53cfa15fe99cc9783b3827
Namatashabira <namatashabira@gmail.com>
2026-07-29 12:58:04 +0300

    Update investor page

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    open: 'microsoft-edge',
+    open: true,
     proxy: {
       '/api': 'http://localhost:3001'
     }
