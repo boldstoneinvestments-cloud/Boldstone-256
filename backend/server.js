5faf19f1117edc76a2278811cbd88a7929095433
Habib-salah <danmugume24@gmail.com>
2026-09-17 21:45:57 +0300

    Fix Express 5 wildcard CORS preflight

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.options('/{*path}', cors(corsOptions))
 app.use(express.json())
 
 if (!process.env.DATABASE_URL) {
